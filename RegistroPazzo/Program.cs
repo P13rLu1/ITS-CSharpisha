100bf79342b225aee57fd2f2ce46560897c09ec3
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-05-22 12:18:15 +0200

    RegistroPazzo.csproj

---
+++ RegistroPazzo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ITS_CSharpisha;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
