32ba1ed3f4ecb6114511dc7e07fd4ec9b7c67ec6
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-05-23 11:53:05 +0200

    aaaaaa

---
+++ RegistroMenoPazzo/Program.cs
@@ -0,0 +1,28 @@
+﻿// Crea una consoleApp per la gestione di un registro elettronico.
+// 1. Visualizza registro
+// 2. Aggiungi studente
+// 2. Modifica studente
+// 3. Cancella Studente
+// 4. Esci
+//
+//
+// VISUALIZZA REGISTRO:
+// - Mostra una lista di tutti gli studenti nel registro
+//     - Opzione per ordinare gli studente nome, o cognome o per ID studente
+//     - possibilità di visualizzare dettagli aggiuntivi per ciascuno studente
+//
+//     AGGIUNGI STUDENTE:
+// - Nome, cognome, data di nascita (dateTime), classe. (genera un ID univoco per lo studente)
+//
+// MODIFICA STUDENTE:
+// - Ricerca dello studente per nome, cognome o ID univoco
+//     - Opzioni per modificare le informazioni come nome, cognome, classe
+//
+// CANCELLA STUDENTE:
+// - Ricerca dello studente per nome, cognome o ID univoco
+//     - Conferma dell'eliminazione prima dell'operazione
+//
+//     Gestisci tutti gli errori ed eccezioni
+using System;
+
+Console.WriteLine("Hello, World!");
