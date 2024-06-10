9ee63f662d288cb3e37ce22f40f7871246755f6e
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-06-10 13:27:56 +0200

    added db integration to consoleapp

--- BigliettApp.ConsoleApp/Program.cs
+++ BigliettApp.ConsoleApp/Program.cs
@@ -1,6 +1,10 @@
-﻿using BigliettApp.ConsoleApp.Controllers;
+﻿using System;
+using BigliettApp.ConsoleApp.Controllers;
+using BigliettApp.DataLayer;
 using BigliettApp.DataLayer.Services;
 using BigliettApp.DataLayer.Stores;
+using BigliettApp.DataLayer.Stores.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BigliettApp.ConsoleApp;
@@ -13,11 +17,31 @@ public class Program
 
         sc.AddScoped<MenuController>();
         sc.AddScoped<ClienteService>();
-        sc.AddSingleton<ClienteStore>();
         sc.AddScoped<SpettacoloService>();
-        sc.AddSingleton<SpettacoloStore>();
+        
+        bool useDb = true;
+        if (useDb)
+        {
+            sc.AddScoped<IClienteStore, ClienteDbStore>();
+            sc.AddScoped<ISpettacoloStore, SpettacoloDbStore>();
+            sc.AddScoped<IPrenotazioneStore, PrenotazioneDbStore>();
+                
+        }
+        else
+        {
+            sc.AddSingleton<IClienteStore, ClienteStore>();
+            sc.AddSingleton<ISpettacoloStore, SpettacoloStore>();
+            sc.AddSingleton<IPrenotazioneStore, PrenotazioneStore>();
+        }
+        
+        sc.AddDbContext<BiglietteriaDbContext>(
+            dbContextOptions => dbContextOptions
+                .UseMySql("server=localhost;port=3306;user=root;password=admin;database=bigliettapp", 
+                    new MySqlServerVersion(new Version(8, 0, 37)))
+        );
         
         var sp = sc.BuildServiceProvider();
         var menuController = sp.GetService<MenuController>();
+        menuController?.Menu();
     }
 }
